f7a47c6e5de3458e498e091f5d311a611e43bb00
sutchan <sutchan@users.noreply.github.com>
2026-05-22 05:53:16 +0000

    feat: 审查项目
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,17 @@
-// @ts-check
-const eslint = require('@eslint/js');
-const {defineConfig} = require('eslint/config');
-const tseslint = require('typescript-eslint');
-const angular = require('angular-eslint');
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-module.exports = defineConfig([
+export default tseslint.config(
   {
     files: ['**/*.ts'],
+    ignores: ['**/*.html'],
     extends: [
       eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended,
+      ...tseslint.configs.recommended,
     ],
-    processor: angular.processInlineTemplates,
     rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
-      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
-  },
-  {
-    files: ['**/*.html'],
-    extends: [
-      angular.configs.templateRecommended,
-      angular.configs.templateAccessibility,
-    ],
-    rules: {},
   }
-]);
+);
